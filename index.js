e074f613d04e5756834779da83401ddd62c535e0
shannoga <shannoga@me.com>
2016-05-27 09:17:53 +0300

    Trying to add parse dashboard

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -20,6 +21,17 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+});
+
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": 'http://shira-dev.herokuapp.com/parse',
+      "appId": 'D6P9U6ClTipkBAuQwolXlc4DiypwVjK9oq9x9pjh',
+      "masterKey": 'ODsesP0PIheUJJAFcONMIeFWfJBHdsse3ZAzCe8q',
+      "appName": "Shira-Dev"
+    }
+  ]
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
