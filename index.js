2ca4ac0db7efb533016d404ec46bd2f4ea6c06a4
shannoga <shannoga@me.com>
2016-05-27 10:57:21 +0300

    Revert "Added parse dashbord"
    This reverts commit 4476d829efeaaf90f96db82e4fec2f33e44e4f50.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-//var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -22,17 +21,6 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
-
-//var dashboard = new ParseDashboard({
-//  "apps": [
-//    {
-//      "serverURL": 'http://shira-dev.herokuapp.com/parse',
-//      "appId": 'D6P9U6ClTipkBAuQwolXlc4DiypwVjK9oq9x9pjh',
-//      "masterKey": 'ODsesP0PIheUJJAFcONMIeFWfJBHdsse3ZAzCe8q',
-//      "appName": "Shira-Dev"
-//    }
-//  ]
-//});
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -45,8 +33,6 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
-
-//app.use('/dashboard', dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
